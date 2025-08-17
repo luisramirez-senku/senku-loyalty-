c20f5783d0af04e56a4d5f83c1ab5c78db5ee852
Luis Diego Ramirez Hernandez <luisdiego.rh1998@gmail.com>
2025-08-17 19:36:29 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,10 +13,9 @@ export const config = {
   // Match only internationalized pathnames
   matcher: [
     // Match all pathnames except for
-    // - … if they start with `/api`, `/_next` or `/_vercel`
+    // - … if they start with `/api`, `/_next/static`, `/_next/image` or `/_vercel`
     // - … the ones containing a dot (e.g. `favicon.ico`)
-    '/((?!api|_next|_vercel|.*\\..*).*)',
+    '/((?!api|_next/static|_next/image|_vercel|.*\\..*).*)',
     '/'
   ]
 };
-
