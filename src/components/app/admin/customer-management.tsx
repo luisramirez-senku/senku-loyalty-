c6390f2fda67f2bb4f6dfd73aedfc2c82208355e
Luis Diego Ramirez Hernandez <luisdiego.rh1998@gmail.com>
2025-08-17 03:56:08 +0000

    ve por ello

--- src/components/app/admin/customer-management.tsx
+++ src/components/app/admin/customer-management.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 
 import { useState } from "react";
@@ -381,3 +382,5 @@ export default function CustomerManagement() {
     </>
   );
 }
+
+    
--- src/components/app/admin/view-history-dialog.tsx
+++ src/components/app/admin/view-history-dialog.tsx
@@ -76,3 +76,5 @@ export function ViewHistoryDialog({
     </Dialog>
   );
 }
+
+    
